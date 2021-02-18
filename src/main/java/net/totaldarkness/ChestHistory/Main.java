09527a8bcebd01ba8c9c15da65c3194aa9e8d88d
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-02-18 01:56:21 -0500

    Add main class to initialize mode

---
+++ src/main/java/net/totaldarkness/ChestHistory/Main.java
@@ -0,0 +1,41 @@
+package net.totaldarkness.ChestHistory;
+
+import net.totaldarkness.ChestHistory.client.events.RenderEvent;
+import net.totaldarkness.ChestHistory.client.gui.ChestGui;
+import net.totaldarkness.ChestHistory.client.gui.ChestHistory;
+import net.totaldarkness.ChestHistory.client.services.ChestGuiService;
+import net.totaldarkness.ChestHistory.client.services.RenderEventService;
+import org.apache.logging.log4j.Logger;
+
+import net.totaldarkness.ChestHistory.client.events.KeyboardEvent;
+import org.apache.logging.log4j.LogManager;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "chesthistory", name = "Chest History", version = "0.0.1", acceptedMinecraftVersions = MinecraftForge.MC_VERSION, clientSideOnly = true)
+public class Main {
+    public static final Minecraft MC = Minecraft.getMinecraft();
+    public static final Logger LOGGER = LogManager.getLogger("chesthistory");
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	LOGGER.info("Starting ChestHistory");
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new KeyboardEvent());
+        MinecraftForge.EVENT_BUS.register(new RenderEventService());
+        MinecraftForge.EVENT_BUS.register(new ChestHistory());
+        MinecraftForge.EVENT_BUS.register(new ChestGuiService());
+    }
+}
