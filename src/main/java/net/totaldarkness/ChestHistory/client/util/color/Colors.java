6efceb6b7703fd6b7cb041dfb4f6646db135cb0e
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-02-18 01:53:52 -0500

    Add color helpers

---
+++ src/main/java/net/totaldarkness/ChestHistory/client/util/color/Colors.java
@@ -0,0 +1,30 @@
+package net.totaldarkness.ChestHistory.client.util.color;
+
+public interface Colors {
+
+    Color WHITE = Color.of(255, 255, 255, 255);
+    Color BLACK = Color.of(0, 0, 0, 255);
+    Color AQUA = Color.of(0, 191, 255, 255);
+    Color DARK_RED = Color.of(127, 0, 0, 255);
+    Color RED = Color.of(255, 0, 0, 255);
+    Color DARK_GREEN = Color.of(0, 127, 0, 255);
+    Color GREEN = Color.of(0, 255, 0, 255);
+    Color LIME = Color.of(191, 255, 0, 255);
+    Color DARK_BLUE = Color.of(0, 0, 170, 255);
+    Color BLUE = Color.of(85, 85, 255, 255);
+    Color ORANGE = Color.of(255, 127, 0, 255);
+    Color GOLD = Color.of(255, 191, 0, 255);
+    Color YELLOW = Color.of(255, 255, 0, 255);
+    Color DARK_PURPLE = Color.of(170, 0, 170, 255);
+    Color PURPLE = Color.of(163, 73, 163, 255);
+    Color MAGENTA = Color.of(255, 0, 255, 255);
+    Color DARK_GRAY = Color.of(85, 85, 85, 255);
+    Color GRAY = Color.of(127, 127, 127, 255);
+    Color LIGHT_GRAY = Color.of(170, 170, 170, 255);
+    Color BROWN = Color.of(153, 102, 51, 255);
+    Color LIGHT_BROWN = Color.of(153, 121, 80, 255);
+    Color DARK_BROWN = Color.of(96, 64, 32, 255);
+
+    Color INVALID = Color.of(-1, -1, -1, -1);
+}
+
