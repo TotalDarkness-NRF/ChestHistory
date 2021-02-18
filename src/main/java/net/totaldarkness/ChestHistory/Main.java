f1ccd5fe9c437afdd49db590739d68a844dd7246
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-02-18 13:11:25 -0500

    Remove some unused items

--- src/main/java/net/totaldarkness/ChestHistory/Main.java
+++ src/main/java/net/totaldarkness/ChestHistory/Main.java
@@ -1,15 +1,9 @@
 package net.totaldarkness.ChestHistory;
 
-import net.totaldarkness.ChestHistory.client.events.RenderEvent;
-import net.totaldarkness.ChestHistory.client.gui.ChestGui;
 import net.totaldarkness.ChestHistory.client.gui.ChestHistory;
 import net.totaldarkness.ChestHistory.client.services.ChestGuiService;
 import net.totaldarkness.ChestHistory.client.services.RenderEventService;
-import org.apache.logging.log4j.Logger;
-
 import net.totaldarkness.ChestHistory.client.events.KeyboardEvent;
-import org.apache.logging.log4j.LogManager;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -19,13 +13,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "chesthistory", name = "Chest History", version = "0.0.1", acceptedMinecraftVersions = MinecraftForge.MC_VERSION, clientSideOnly = true)
 public class Main {
-    public static final Minecraft MC = Minecraft.getMinecraft();
-    public static final Logger LOGGER = LogManager.getLogger("chesthistory");
-
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-    	LOGGER.info("Starting ChestHistory");
-    }
+    public void preInit(FMLPreInitializationEvent event) {}
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
